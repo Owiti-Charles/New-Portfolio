6d584a106b4d36c07723fd5a27b1c9ddc51128a6
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-11-11 11:01:45 +0300

    Update my skills

--- js/main.js
+++ js/main.js
@@ -94,10 +94,10 @@
     var typed_strings = $('.text-slider-items').text();
 		var typed = new Typed('.text-slider', {
 			strings: typed_strings.split(','),
-			typeSpeed: 80,
+			typeSpeed: 100,
 			loop: true,
-			backDelay: 1100,
-			backSpeed: 30
+			backDelay: 1800,
+			backSpeed: 50
 		});
 	}
 
